95c93446f225a6810666e897a07b1a16f94b3ce6
liuzk <sdhzlzhk@126.com>
2022-11-13 23:18:29 +0800

    feat:提交作完的题目

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,18 +10,24 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut new_vec = self.clone();
+        new_vec.push(String::from("Bar"));
+        new_vec
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn is_vec_pop_eq_bar() {
         let mut foo = vec![String::from("Foo")].append_bar();
