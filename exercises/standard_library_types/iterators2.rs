79850df9f14932c525aa8080db612b1cf324f9b1
liuzk <sdhzlzhk@126.com>
2022-11-15 01:48:12 +0800

    feat:提交作完的题目

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,7 +11,9 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            format!("{}{}",first.to_uppercase().to_string(),c.collect::<String>())
+        }
     }
 }
 
@@ -21,7 +22,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut new_result = vec![];
+    for word in words.iter() {
+        new_result.push(capitalize_first(word));
+    }
+    new_result
 }
 
 // Step 3.
@@ -29,7 +34,8 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let rsult = capitalize_words_vector(words);
+    rsult.join("").to_string()
 }
 
 #[cfg(test)]
